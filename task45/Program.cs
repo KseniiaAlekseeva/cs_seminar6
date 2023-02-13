6fb201d9b4c938f1c5637852139059dfae072413
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-13 21:47:19 +0300

    Add task45

---
+++ task45/Program.cs
@@ -0,0 +1,31 @@
+﻿// Function that copies the array
+
+int[] CreateRandomArray(int size, int minVal, int maxVal)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(minVal, maxVal + 1);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+    Console.WriteLine();
+}
+
+int[] CopyArray(int[] array)
+{
+    int[] copy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+        copy[i] = array[i];
+    return copy;
+}
+
+int size = 12;
+int[] arr = CreateRandomArray(size, -10, 10);
+PrintArray(arr);
+Console.WriteLine();
+int[] copy = CopyArray(arr);
+PrintArray(copy);
