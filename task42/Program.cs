32d75affec942380cac5529d50a604e0bd1d41d5
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-13 21:23:28 +0300

    Add task42

---
+++ task42/Program.cs
@@ -0,0 +1,23 @@
+﻿// Convert a decimal number to binary
+
+int ConvertToBinary(int num)
+{
+    string str = "";
+    int curnum = num;
+
+    do
+    {
+        str = Convert.ToString(curnum % 2) + str;
+        curnum = curnum / 2;
+    }
+    while (curnum != 0);
+
+    int binum = Convert.ToInt32(str);
+    return binum;
+}
+
+Console.WriteLine("Enter the decimal integer number: ");
+int num = int.Parse(Console.ReadLine());
+
+int binum = ConvertToBinary(num);
+Console.WriteLine(binum);
