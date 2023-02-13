22d314df2518951b7ae994300f802eee763ceb3f
KseniiaAlekseeva <csushaalekseeva@gmail.com>
2023-02-13 20:28:21 +0300

    Add task39

---
+++ task39/Program.cs
@@ -0,0 +1,36 @@
+﻿// Reverse an array
+
+int[] CreateRandomArray(int size, int minVal, int maxVal)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(minVal, maxVal + 1);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + ", ");
+    Console.WriteLine();
+}
+
+void ReverseArray(int[] array)
+{
+    int cur = 0;
+    int N = array.Length;
+    for (int i = 0; i < N / 2; i++)
+    {
+        cur = array[i];
+        array[i] = array[N - i - 1];
+        array[N - i - 1] = cur;
+    }
+}
+
+int size = 12;
+int[] arr = CreateRandomArray(size, -10, 10);
+PrintArray(arr);
+Console.WriteLine();
+ReverseArray(arr);
+PrintArray(arr);
+
